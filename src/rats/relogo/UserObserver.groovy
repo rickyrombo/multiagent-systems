16f3b3add08a543b6e77a5fb6722c525e8b7bfa1
Marcus Pasell <marcus.pasell@hudl.com>
2016-04-11 06:31:31 -0500

    tried to add respawn of food and failed

--- src/rats/relogo/UserObserver.groovy
+++ src/rats/relogo/UserObserver.groovy
@@ -20,48 +20,65 @@ class UserObserver extends ReLogoObserver{
 		createRats(5){
 			setColor(blue());
 			sex = Rat.Sex.MALE;
-			if(random(1) == 0) {
-				setxy(randomXcor(), getAvenueY(random(1)))
-			} else {
-				setxy(getStreetX(random(2)), randomYcor());
-			}
-			setHeading(random(3) * 90);
+			setupMovement(it);
 			aggressivenessConstant = Math.max(0 , Math.min(1, randomNormal(0.5, 0.166666)));
 		}
 		createRats(5){
 			setColor(pink());
 			sex = Rat.Sex.FEMALE;
-			if(random(1) == 0) {
-				setxy(randomXcor(), getAvenueY(random(1)))
-			} else {
-				setxy(getStreetX(random(2)), randomYcor());
-			}
-			setHeading(random(3) * 90);
+			setupMovement(it);
 			aggressivenessConstant = Math.max(0 , Math.min(1, randomNormal(0.5, 0.166666)));
 		}
 		for (int i = 0; i < 6; i++){
 			createFood(1){
-				def xFactor = Math.floor(i/2);
-				def yFactor = i%2;
-				def x = getStreetX(xFactor);
-				def y = getAvenueY(yFactor);
-				setxy(x, y);
+				def intersection = getIntersection(i);
+				setxy(intersection.x, intersection.y);
 			}
 		}
 	}
 	
+	def setupMovement(Rat rat){
+		if(random(2) == 0) {
+			rat.setxy(randomXcor(), getAvenueY(random(2)))
+			rat.setHeading(random(2) * 90);
+		} else {
+			rat.setxy(getStreetX(random(3)), randomYcor());
+			rat.setHeading(random(2) * 180);
+		}
+	}
+	
+	def getIntersection(i) {
+		return [x: getStreetX(Math.floor(i/2)), y: getAvenueY(i % 2)];
+	}
+	
 	def getStreetX(streetNumber) {
-		return worldWidth() / 3.0 * (streetNumber + 1) + getMinPxcor() + worldWidth() / 6.0;
+		return worldWidth() / 3.0 * (streetNumber) + getMinPxcor() + worldWidth() / 6.0;
 	}
 	
 	def getAvenueY(avenueNumber) {
-		return worldHeight() / 2.0 * (avenueNumber + 1) + getMinPycor() + worldHeight() / 4.0;
+		return worldHeight() / 2.0 * (avenueNumber) + getMinPycor() + worldHeight() / 4.0;
 	}
 	
 	@Go
 	def go() {
 		ask(rats()){
 			step();
+		}
+		tick();
+		if (ticks() % 10 == 0) {
+			def seed = random(6);
+			def i = seed;
+			def intersection = getIntersection(i);
+			//!anyQ(filter({ intersection.x == it.getXcor() && intersection.y == it.getYcor() }, food()))
+			//foodOn(patch(intersection.x, intersection.y)) > 0
+			while (anyQ(food()) && !anyQ(filter({ intersection.x == it.getXcor() && intersection.y == it.getYcor() && it }, food())) && ++i % 6 != seed) {
+				intersection = getIntersection(i % 6);
+			}
+			if (i == seed || i % 6 != seed) {
+				this.createFood(1){
+					setxy(intersection.x, intersection.y);
+				}
+			}
 		}
 	}
 	
