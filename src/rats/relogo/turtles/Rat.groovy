943b8621ab3f6d8c17e153d8fc11b4d7576d8d05
Marcus Pasell <marcus.pasell@hudl.com>
2016-04-11 00:27:02 -0500

    first properties

--- src/rats/relogo/turtles/Rat.groovy
+++ src/rats/relogo/turtles/Rat.groovy
@@ -12,4 +12,21 @@ import repast.simphony.relogo.schedule.Setup;
 
 class Rat extends ReLogoTurtle {
 
+	def age = 0;
+	def timeSinceLastMeal = 0;
+	def agressivenessConstant;
+	
+	def step() {
+		age++;
+		timeSinceLastMeal++;
+	}
+	
+	def hungerFactor() {
+		return timeSinceLastMeal / 1000;
+	}
+	
+	def sexualDesireFactor() {
+		return age < 25000 ? 0 : 0.5 + 0.00004 * age;
+	}
+	
 }
